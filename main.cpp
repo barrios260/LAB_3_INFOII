3b9df4fc9c748613b48854b4adaaceb0f7c68498
barrios260 <95543529+barrios260@users.noreply.github.com>
2022-07-17 03:27:34 -0500

    inicio lab 3

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+//crear usuarios
+void create_user(string );
+//validar clave
+void validate_pass(string);
+int main()
+{
+    int choose;
+    cout<<"elija de que manera quiere ingresar."<<endl;
+    cout<<"1. para usuario."<<endl;
+    cout<<"2. para administrador"<<endl;
+    switch(choose){
+    case 1:{
+    }
+
+        break;
+    case 2:{
+    }
+        break;
+    }
+}
+void create_user(string){
+
+}
+
+void validate_pass(string){
+
+}
