f493a7e613b630467eb029508146000b6b058269
barrios260 <95543529+barrios260@users.noreply.github.com>
2022-07-18 00:52:57 -0500

    "inicios del lab, error funcion de cedula"

--- main.cpp
+++ main.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
 #include <string.h>
+#include <fstream>
+#include <sstream>
+
+
 using namespace std;
 //crear usuarios
-void create_user(string );
+void create_user(int );
 //validar clave
 void validate_pass(string);
+
 int main()
 {
     int choose;
-    cout<<"elija de que manera quiere ingresar."<<endl;
-    cout<<"1. para usuario."<<endl;
-    cout<<"2. para administrador"<<endl;
+    cout<<"---elija como quiere ingresar.---"<<endl;
+    cout<<"1. como administrador."<<endl;
+    cout<<"2. como usuario."<<endl;
+    cin>>choose;
+
     switch(choose){
     case 1:{
+        int cedula;
+        create_user(cedula);
+
     }
+
 
         break;
     case 2:{
@@ -21,8 +32,17 @@ int main()
         break;
     }
 }
-void create_user(string){
-
+void create_user(int){
+    cout<<"ingrese el numero de cedula: ";
+    int cedula;
+    cin>>cedula;
+    try{
+    if(isalpha(cedula))
+       cout<<"cedula ingresada correctamente. ";
+    }
+    catch(int cedula){
+        cout<<"solo debes ingresar numeros...";
+    }
 }
 
 void validate_pass(string){
