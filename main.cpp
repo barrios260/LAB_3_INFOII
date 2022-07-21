d1788d5ccf316b809f3c1f1046c751920791b5a2
barrios260 <95543529+barrios260@users.noreply.github.com>
2022-07-21 00:44:20 -0500

    funcion creada para pasar datos a binario
    se crea una funcion para pasar los datos a binario aun sin encriptar

--- main.cpp
+++ main.cpp
@@ -3,12 +3,14 @@
 #include <fstream>
 #include <sstream>
 #include <cstring>
-
+#include <bitset>
 using namespace std;
 //crear usuarios
 void create_user(string);
 //validar clave
 void validate_pass(string);
+//funcion a binario
+string TextToBinaryString(string words);
 
 int main()
 {
@@ -36,7 +38,7 @@ int main()
 //funcion que permite al administrador crear un usuario
 void create_user(string){
 
-    string cedula,saldo,clave;
+    string cedula,saldo,clave,id,pass,mon,datos1;
     int i=0;
     try {                                       //try catch que valida solo el ingreso de numeros en la cedula
         cout<<"ingrese el numero de cedula: ";
@@ -47,21 +49,33 @@ void create_user(string){
         }
         char c=',',p='.';
         cedula.append(1,c );//add a la cedula una coma al final para codificar el archivo
+        id=TextToBinaryString(cedula);
         cout<<"digite la clave para el usuario: "<<cedula<<endl;
         cin>>clave;
         clave.append(1,c);//add a la clave coma al final para codificar archivo
+        pass=TextToBinaryString(clave);
         cout<<"digite el saldo para el usuario: "<<cedula<<endl;
         cin>>saldo;
 
         saldo.append(1,p);
-        cout<<"los datos ingresados fueron: "<<endl<<cedula<<clave<<saldo<<endl;
+        mon=TextToBinaryString(saldo);
     }
     catch(char c) {
         if(c=='1')
         cout<<"sr. admin solo puede ingresar numeros en la cedula..."<<endl;
     }
-
+datos1=id+pass+mon;
+cout<<datos1<<endl;
 }
+
 void validate_pass(string){
 
 }
+string TextToBinaryString(string words) {
+    string binaryString = "";
+    for (char& _char : words) {
+        binaryString +=bitset<8>(_char).to_string();
+    }
+    return binaryString;
+}
+
